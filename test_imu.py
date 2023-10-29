a493a6d10fdd3336e672b76d64d239640d42e354
Bogdan Costea <84265918+BogdanCostea0@users.noreply.github.com>
2023-10-29 23:45:33 +0200

    few chan

--- test_imu.py
+++ test_imu.py
@@ -1,21 +1,25 @@
-from machine import Pin, I2C
+from imu import MPU6050
 from time import sleep
+from machine import Pin, I2C
 
-from mpu6050 import MPU6050
+#Shows Pi is on by turning on LED when plugged in
+LED = Pin("LED", Pin.OUT)
+LED.on()
 
-# setup i2c bus 1
+from imu import MPU6050
+from time import sleep
+from machine import Pin, I2C
 
-i2c_1 = I2C(id=1, sda=Pin(26), scl=Pin(27))
-
-# setup mpu6050
-
-mpu = MPU6050()
+i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=400000)
+imu = MPU6050(i2c)
 
 while True:
-    print(mpu.get_gyro_raw())
-    sleep(0.1)
-    print('am ajuns aici')
-
-
-
-
+    ax=round(imu.accel.x,2)
+    ay=round(imu.accel.y,2)
+    az=round(imu.accel.z,2)
+    gx=round(imu.gyro.x)
+    gy=round(imu.gyro.y)
+    gz=round(imu.gyro.z)
+    tem=round(imu.temperature,2)
+    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
+    sleep(0.2) 

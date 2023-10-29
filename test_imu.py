8732922ecd92948c618affd0d42c06f54ada55bd
Bogdan Costea <84265918+BogdanCostea0@users.noreply.github.com>
2023-10-30 00:10:36 +0200

    Update test_imu.py

--- test_imu.py
+++ test_imu.py
@@ -1,10 +1,12 @@
 from imu import MPU6050
 from time import sleep
 from machine import Pin, I2C
-
+from math import atan2, degrees, pi
 #Shows Pi is on by turning on LED when plugged in
 LED = Pin("LED", Pin.OUT)
 LED.on()
+
+RAD_To_DEG = 180/pi
 
 from imu import MPU6050
 from time import sleep
@@ -12,6 +14,14 @@ from machine import Pin, I2C
 
 i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=400000)
 imu = MPU6050(i2c)
+
+def calculate(xacel, yacel):
+    AccXangle = (float) (atan2(*(xacel+1),*(xacel+2))+pi) * RAD_To_DEG
+    AccYangle = (float) (atan2(*(yacel+2),*yacel)+pi) * RAD_To_DEG
+
+    return AccXangle, AccYangle
+
+
 
 while True:
     ax=round(imu.accel.x,2)
@@ -21,5 +31,11 @@ while True:
     gy=round(imu.gyro.y)
     gz=round(imu.gyro.z)
     tem=round(imu.temperature,2)
-    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
+
+    calculate(imu.accel.x, imu.accel.y)
+
+
+    
+
+    # print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem)
     sleep(0.2) 
